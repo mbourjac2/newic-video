104be98734fe701fa28f3b8fb6c85684a0d087ea
Michael Bourjac <michael@agence-newic.com>
2024-02-13 10:18:02 +0100

    chore: move css and js files to src folder

--- wp-content/themes/newic-video/rollup.config.mjs
+++ wp-content/themes/newic-video/rollup.config.mjs
@@ -1,11 +1,11 @@
-import terser from "@rollup/plugin-terser";
+import terser from '@rollup/plugin-terser';
 
 export default {
-  input: "static/js/index.js",
+  input: 'src/js/index.js',
   output: [
     {
-      file: "static/bundle.min.js",
-      format: "es",
+      file: 'static/bundle.min.js',
+      format: 'es',
       plugins: [terser()],
     },
   ],

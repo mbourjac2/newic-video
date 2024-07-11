0e19bb13947ad3b9bb4d582a9bb4ad44c27d6233
Michael Bourjac <michael@agence-newic.com>
2024-07-11 16:59:00 +0200

    feat: display random project suggestions

--- wp-content/themes/newic-video/single-project.php
+++ wp-content/themes/newic-video/single-project.php
@@ -12,10 +12,19 @@ $context = Timber::context();
 $context['project'] = Timber::get_post();
 
 if (is_single() && $context['project']->post_type === 'project') {
+    // Query for four random projects
+    $args = array(
+        'post_type' => 'project',
+        'posts_per_page' => 4,
+        'orderby' => 'rand',
+        'post__not_in' => array($context['project']->ID) // Exclude the current project
+    );
+
+    $random_projects = Timber::get_posts($args);
+    $context['random_projects'] = $random_projects;
+
     Timber::render('pages/single-project/single-project.twig', $context);
 } else {
     // Optionally handle cases where the post type is not 'project'
     Timber::render('pages/404/404.twig', $context);
 }
-
-
